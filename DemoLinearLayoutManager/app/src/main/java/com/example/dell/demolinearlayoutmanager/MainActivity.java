1ae224417c6e974431fb9133a582a3d4ae27f5cc
HaVuNguyen <32772746+HaVuNguyen@users.noreply.github.com>
2018-04-05 11:11:03 +0700

    Edit demo

--- DemoLinearLayoutManager/app/src/main/java/com/example/dell/demolinearlayoutmanager/MainActivity.java
+++ DemoLinearLayoutManager/app/src/main/java/com/example/dell/demolinearlayoutmanager/MainActivity.java
@@ -6,12 +6,13 @@ import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
+import android.view.View;
 
 import java.util.ArrayList;
 import java.util.List;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private RecyclerView rvMusic;
     private MusicApdapter mMusicAdapter;
     private List<Music> mmusic;
@@ -50,34 +51,49 @@ public class MainActivity extends AppCompatActivity {
         mMusicAdapter = new MusicApdapter(this, mmusic);
         rvMusic.setAdapter(mMusicAdapter);
 
-        //cuộn theo chiều ngang
+        //etgiatri = (EditText) findViewById(R.id.ketqua);
+        int[] idButton = {R.id.lh, R.id.lv, R.id.sh, R.id.sv, R.id.gh, R.id.gv};
+        for (int id:idButton){
+            View v = (View)findViewById(id);
+            v.setOnClickListener(this);
+        }
 
-       //LinearLayoutManager linearLayoutManager1 = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
-       //rvMusic.setLayoutManager(linearLayoutManager1);
-
-        //cuộn theo chiều dọc
-
-        LinearLayoutManager linearLayoutManager2 = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
-        rvMusic.setLayoutManager(linearLayoutManager2);
-
-        // cuộn theo dạng sole theo chiều dọc
-
-        //StaggeredGridLayoutManager staggeredGridLayoutManager = new StaggeredGridLayoutManager(4,StaggeredGridLayoutManager.VERTICAL);
-        //rvMusic.setLayoutManager(staggeredGridLayoutManager);
-
-        // cuộn theo dạng sole theo chiều ngang
-
-        //StaggeredGridLayoutManager staggeredGridLayoutManager2 = new StaggeredGridLayoutManager(4,StaggeredGridLayoutManager.HORIZONTAL);
-        //rvMusic.setLayoutManager(staggeredGridLayoutManager2);
-
-        // cuộn theo dạng lưới chiều ngang
-
-        //GridLayoutManager gridLayoutManager = new GridLayoutManager(this,10,GridLayoutManager.HORIZONTAL,false);
-        //rvMusic.setLayoutManager(gridLayoutManager);
-
-        // cuộn theo dạng lướt chiều dọc
-        //GridLayoutManager gridLayoutManager2 = new GridLayoutManager(this,3,GridLayoutManager.VERTICAL,false);
-        //rvMusic.setLayoutManager(gridLayoutManager2);
 }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.lh :
+                LinearLayoutManager linearLayoutManager1 = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+                rvMusic.setLayoutManager(linearLayoutManager1);
+                break;
+
+            case R.id.lv :
+                LinearLayoutManager linearLayoutManager2 = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+                rvMusic.setLayoutManager(linearLayoutManager2);
+                break;
+
+            case R.id.sh :
+                StaggeredGridLayoutManager staggeredGridLayoutManager2 = new StaggeredGridLayoutManager(4,StaggeredGridLayoutManager.HORIZONTAL);
+                rvMusic.setLayoutManager(staggeredGridLayoutManager2);
+                break;
+
+            case R.id.sv :
+                StaggeredGridLayoutManager staggeredGridLayoutManager = new StaggeredGridLayoutManager(4,StaggeredGridLayoutManager.VERTICAL);
+                rvMusic.setLayoutManager(staggeredGridLayoutManager);
+                break;
+
+            case R.id.gh:
+                GridLayoutManager gridLayoutManager = new GridLayoutManager(this,10,GridLayoutManager.HORIZONTAL,false);
+                rvMusic.setLayoutManager(gridLayoutManager);
+                break;
+
+            case R.id.gv:
+                GridLayoutManager gridLayoutManager2 = new GridLayoutManager(this,3,GridLayoutManager.VERTICAL,false);
+                rvMusic.setLayoutManager(gridLayoutManager2);
+                break;
+        }
+
+    }
 }
 
